2c1a163d8633f46337cdddb0691c75de19498559
rlane85 <ryanlane1985@gmail.com>
2022-03-17 01:04:02 +0000

    initialize connections to smartthings and my pws data servers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+// import Messages from './Messages';
+// import MessageInput from './MessageInput';
+
+// import "./App.css";
 
 function App() {
+  const [pws, setPws] = useState({
+    connected: false,
+    epoch: { value: false },
+  });
+
+  const [st, setSt] = useState({
+    connected: false,
+  });
+
+  const [user, setUser] = useState("Ryan");
+
+  useEffect(() => {
+    const pwsSocket = io(process.env.REACT_APP_PWS_SOCKET_DOMAIN + "/pushSocket");
+
+    const stSocket = io(process.env.REACT_APP_ST_SOCKET_DOMAIN + "/stSocket", {
+      forceNew: true,
+      path: "/stSocket",
+      transports: ["websocket", "polling"],
+    });
+    
+console.log(`${JSON.stringify(process.env, null, 1)} process.env App.js`)
+    stSocket.emit("getConnectionPacket", user);
+    stSocket.on("connect", () => {
+      console.log("connected to stSocket");
+    });
+    stSocket.on("connectionPacket", (msg) => {
+      console.log("🚀 ~ file: App.js ~ line 32 ~ newStSocket.on ~ msg", msg);
+      setSt({
+        ...st,
+        ...msg,
+      });
+    });
+
+    pwsSocket.on("connectionError", (err) => {
+      console.log("🚀 ~ file: App.js ~ line 42 ~ pwsSocket.on ~ err", err);
+    });
+    pwsSocket.on("connect", () => {
+      console.log("connected to pwsSocket");
+      setPws({
+        ...pws,
+        connected: true,
+      });
+    });
+    pwsSocket.on("disconnect", () => {
+      console.log("disconnected from pwsSocket");
+    });
+    pwsSocket.on("rapidWind", (msg) => {
+      setPws({
+        ...pws,
+        ...msg,
+      });
+    });
+    pwsSocket.on("packetObject", (msg) => {
+      setPws({
+        ...pws,
+        ...msg,
+      });
+    });
+
+    return () => {
+      pwsSocket.close();
+      stSocket.close();
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {pws.epoch.value ? pws.epoch.value : "not connected"}
     </div>
   );
 }
