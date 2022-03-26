b161e269b7b4a44b2c6f22fedfb9315119b9cb35
rlane85 <ryanlane1985@gmail.com>
2022-03-26 14:25:35 +0000

    comments

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,15 +6,23 @@ const round = num => {
 
 	return data ? (
 		<div>
-		{Object.keys(data).map(objKey => {
-			const { unitTitle, value, title, originUnit, convertUnit } = data[objKey]
-			const convertValue = (originUnit !== convertUnit) ? round(convert(value).from(originUnit).to(convertUnit)) : value;
-			const realValue = (title === "Time") ? new Date(value * 1000).toLocaleString() : convertValue
-
-			return (
-				<p key={title}> {title} {realValue} {convertUnit}</p>
-			)
-		})}
+			{Object.keys(data).map(objKey => {
+				//destructure thr object
+				const { unitTitle, value, title, originUnit, convertUnit } = data[objKey];
+				//is there an indication the value needs to be converted?
+				const convertValue = (originUnit !== convertUnit) ? (
+				//if so round the conversion
+					round(convert(value).from(originUnit).to(convertUnit))
+					//if not, give raw value
+				) : value
+				
+				//convert to time if needed
+				const realValue = (title === "Time") ? new Date(value * 1000).toLocaleString() : convertValue
+	
+				return (
+					<p key={title}> {title} {realValue} {convertUnit}</p>
+				)
+			})}
 		</div>
 	) : (
 	<p>Waiting for data</p>)
