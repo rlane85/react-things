4e32aa1708bf84266cabce4ae63a5ecb21e0ccd7
rlane85 <ryanlane1985@gmail.com>
2022-04-05 02:20:45 +0000

    pws weather as entry point

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import UserService from "../services/user.service";
 
 const Home = ({data}) => {
-  const [content, setContent] = useState("");
 
-  useEffect(() => {
-    UserService.getPublicContent().then(
-      (response) => {
-		  const placeholderData = "Our Meterological Observations"
-        setContent(placeholderData);
-      },
-      (error) => {
-        const _content =
-          (error.response && error.response.data) ||
-          error.message ||
-          error.toString();
+  const { PublicContent } = UserService;
 
-        setContent(_content);
-      }
-    );
-  }, []);
-
+  
   return (
     <div className="container">
       <header className="jumbotron">
-        <h3>{content}</h3>
-		<div>{data.pws}</div>
+	  	<PublicContent />
       </header>
     </div>
   );
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,11 +1,51 @@
 import axios from "axios";
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
 import authHeader from "./auth-header";
 import { Weather } from "../routes/pws.js"
 
 const API_URL = process.env.REACT_APP_AUTH_DOMAIN +  "/api/test/";
 
-const getPublicContent = () => {
-  return axios.get(API_URL + "all");
+const PublicContent = () => {
+  const [pws, setPws] = useState(false);
+
+  const updatePws = (msg) => {
+    setPws((pws) => ({
+      ...pws,
+      ...msg,
+    }));
+	};
+  useEffect(() => {
+    const pwsSocket = io(
+      process.env.REACT_APP_PWS_SOCKET_DOMAIN + "/pwsSocket",
+      {
+        forceNew: true,
+        path: "/pwsSocket",
+        transports: ["websocket", "polling"],
+      }
+	  );
+    pwsSocket.on("connectionError", (err) => {
+      console.log("🚀 ~ file: App.js ~ line 42 ~ pwsSocket.on ~ err", err);
+    });
+    pwsSocket.on("connect", () => {
+      console.log("connected to pwsSocket");
+    });
+
+    pwsSocket.on("disconnect", () => {
+      console.log("disconnected from pwsSocket");
+    });
+
+    pwsSocket.on("data", (msg) => {
+      updatePws(msg);
+    });
+
+    return () => {
+      pwsSocket.close();
+    };
+}, []);
+
+	return <Weather data={pws} />
+
 };
 
 const getUserBoard = () => {
@@ -21,7 +61,7 @@ const getAdminBoard = () => {
 };
 
 const UserService = {
-  getPublicContent,
+  PublicContent,
   getUserBoard,
   getModeratorBoard,
   getAdminBoard,
