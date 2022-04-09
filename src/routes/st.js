bc37ed504a408f4279d6ac1f42ad5cad6b152fa6
rlane85 <ryanlane1985@gmail.com>
2022-04-09 01:09:10 +0000

    prettier

--- src/routes/st.js
+++ src/routes/st.js
@@ -1,15 +1,12 @@
-
 export const SmartThings = ({ data }) => {
   const { devices } = data;
-	
+
   return (
     <div>
-			{Object.keys(devices).map((deviceId) => {
+      {Object.keys(devices).map((deviceId) => {
         return (
           <div key={deviceId}>
-            <button key={deviceId}>
-              {devices[deviceId].name}
-            </button>
+            <button key={deviceId}>{devices[deviceId].name}</button>
           </div>
         );
       })}
