3002defc41885ab12d1c8db5bd613786ae81c57c
rlane85 <ryanlane1985@gmail.com>
2022-05-14 18:28:49 +0000

    prettier

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,13 +1,13 @@
-import { Device } from "./smartThings"
+import { Device } from "./smartThings";
 
-export const Panel = ({panel, devices}) => {
-	const { tiles } = panel;
-	return (
-		<div>
-			<h3>{panel.label}</h3>
-			{tiles.map(tileId => {
-				return<Device key={tileId} device={devices[tileId]} />
-			})}
-		</div>
-	)
-}
+export const Panel = ({ panel, devices }) => {
+  const { tiles } = panel;
+  return (
+    <div>
+      <h3>{panel.label}</h3>
+      {tiles.map((tileId) => {
+        return <Device key={tileId} device={devices[tileId]} />;
+      })}
+    </div>
+  );
+};
