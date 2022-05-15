d2a3e863f1c90fa692a6455c52f5117aee8fb994
rlane85 <ryanlane1985@gmail.com>
2022-05-15 02:34:43 +0000

    rename key for clarity

--- src/sockets/smartthings/emits.js
+++ src/sockets/smartthings/emits.js
@@ -1,9 +1,9 @@
 import { stSocket } from "./index";
 export const deviceCommand = (deviceId, capability, command, args) => {
-  console.log("%cemits.js line:3 command", "color: #007acc;", command);
   stSocket.emit("deviceCommand", {
-    device: deviceId,
+    deviceId: deviceId,
     capability: capability,
     command: command,
+    args: args,
   });
 };
