f8361139ead8aba6d535179bfe44d3106caa3ff8
rlane85 <ryanlane1985@gmail.com>
2022-05-18 00:51:23 +0000

    let server decide command

--- src/components/smartThings/deviceTypes/Actuator.js
+++ src/components/smartThings/deviceTypes/Actuator.js
@@ -3,12 +3,11 @@ import { deviceCommand } from "../../../sockets/smartthings/emits.js";
 import { Switch } from "@mui/material";
 export const Actuator = ({ value, id, attribute, awaitingReply }) => {
   const newValue = value === "on" ? "off" : "on";
-  const checked = value === "on" ? true : false;
   return (
     <Switch
       disabled={awaitingReply}
       checked={value === "on"}
-      onClick={() => deviceCommand(id, attribute, newValue)}
+      onClick={() => deviceCommand(id, attribute)}
     />
   );
 };
