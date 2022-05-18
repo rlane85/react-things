8e6cd9709964ac2273fb474863bc0aea76bcadf8
rlane85 <ryanlane1985@gmail.com>
2022-05-18 00:53:09 +0000

    working on socket context

--- src/sockets/smartthings/index.js
+++ src/sockets/smartthings/index.js
@@ -4,11 +4,16 @@ import { socketEvents } from "./events";
 export const stSocket = io(
   process.env.REACT_APP_ST_SOCKET_DOMAIN + "/stSocket",
   {
-    forceNew: true,
     path: "/stSocket",
     transports: ["websocket", "polling"],
   }
 );
-export const initSockets = ({ setStValue }) => {
-  socketEvents({ setStValue });
+export const initSockets = (setStValue, connected) => {
+  socketEvents(setStValue);
+  if (!connected) {
+    // console.log("%cindex.js line:14 connected", "color: #007acc;", connected);
+    stSocket.connect();
+  }
+
+  // console.log(stSocket);
 };
