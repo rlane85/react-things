163296cc5ea65335848c4471ec8ad6bd6621ea1e
rlane85 <ryanlane1985@gmail.com>
2022-05-18 00:51:39 +0000

    disambiguation of attributes and capabilities

--- src/components/smartThings/deviceTypes/Dimmer.js
+++ src/components/smartThings/deviceTypes/Dimmer.js
@@ -1,20 +1,22 @@
 import { useState } from "react";
 import { deviceCommand } from "../../../sockets/smartthings/emits.js";
-
+import styles from "./deviceTypes.module.css";
 import { Slider } from "@mui/material";
 export const Dimmer = ({ value, id, attribute, awaitingReply }) => {
   // console.log(value);
+
   const [sliderValue, setSliderValue] = useState(value);
   const handleChange = (event, newValue) => {
     setSliderValue(newValue);
   };
   const handleChangeCommit = (event, newValue) => {
     setSliderValue(newValue);
-    deviceCommand(id, "switchLevel", "setLevel", newValue);
+    deviceCommand(id, "level", newValue);
   };
   return (
     <Slider
       value={sliderValue}
+      classes={{ thumb: styles.thumb }}
       color={"secondary"}
       step={10}
       marks
